f99afa6ddf9f3e7208ab98dd2d8f83fcb11dabea
jaypandya594 <74663733+jaypandya594@users.noreply.github.com>
2026-05-31 16:40:11 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,7 +2,7 @@ def call(String Project, String ImageTag, String dockerhubuser){
    echo "This is pushing the image to Docker Hub"
    withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]){
     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-    sh "docker image tag react-notes-app:latest jaypandya1234/react-notes-app:latest"
-    sh "docker push jaypandya1234/react-notes-app:latest "
+    sh "docker image tag ${Project}:${ImageTag} ${dockerhubuser}/${Project}:${ImageTag}"
+    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
     }
 }
