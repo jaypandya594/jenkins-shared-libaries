2599d063cb2194484eab4ae6fd7870a21c9e3774
jaypandya594 <74663733+jaypandya594@users.noreply.github.com>
2026-05-31 16:29:31 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+def call(String projectName, String ImageTag, String DockerHubUser){
  
   sh "docker build -t "${DockerHubUser}"/"${projectName}":"${ImageTag}"
   
